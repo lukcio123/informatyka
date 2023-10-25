cd90bb6a38f72f2f98014557a6cc32647572b933
lukcio123 <lukaszizotow76@gmail.com>
2023-10-25 11:50:46 +0200

    Komentarz

--- lekcja 1/Untitled1.cpp
+++ lekcja 1/Untitled1.cpp
@@ -1,7 +1,23 @@
  #include <iostream>
  using namespace std;
- int main(){
-cout <<"Hello world"<<endl;
-cout << "Imie"<< endl;
-    return 0;
+ int main()
+ {  
+ 
+    short liczba =2; // zmienna liczbowa
+    float liczbauzytkownika;
+    cout <<"Hello world"<<endl;
+    cout << "Imie" << liczba; // wyświetlenie zmiennej
+    
+    cout << endl; // koniec lini
+    cout << "Podaj liczba: ";
+	cin >> liczbauzytkownika; // wprowadzenie danych i zapisanie do zmiennej 
+	
+	cout << endl;
+	cout << "Podana liczba: " << liczbauzytkownika;
+	
+	/*
+	komentarz
+	na wiele lini
+	*/
+	return 0;
 }
