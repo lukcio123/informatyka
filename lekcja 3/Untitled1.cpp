6e15bad799479ce3e67dd821c16008704fc2ae92
lukcio123 <lukaszizotow76@gmail.com>
2023-11-08 11:54:39 +0100

    Komentarz

---
+++ lekcja 3/Untitled1.cpp
@@ -0,0 +1,25 @@
+ #include <iostream>
+ using namespace std;
+ 
+ int main(){
+  for(int i = 1; i<=2; i++){
+  	cout <<endl;
+  for(int j = 0; j <11; j++){
+  	int k = i *j;
+  cout << i<< "*" <<j << "=" <<k<< endl;
+ 
+}
+}
+ //zadanie2
+ int w,h;
+ cout << "Podaj Wysokosc: " << endl;
+ cin >> w;
+ cout << "Podaj Szerokosc: " << endl;
+ cin >> h;
+ 
+ 	
+	 cout << endl;
+   system("pause");
+ 	
+ 	
+ }
