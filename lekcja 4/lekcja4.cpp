2404f9a7c4c40141f48fea29260e16eba833ddb6
lukcio123 <lukaszizotow76@gmail.com>
2023-11-20 14:59:51 +0100

    Komentarz

---
+++ lekcja 4/lekcja4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+ // petla while
+ // while(warunek konczacy)
+ short a = 30;
+ while(a > 0){
+ 	cout << a << ", ";
+ 	a -= 1;
+ 	
+ }
+cout << endl;
+
+ short liczba = 0;
+ while(liczba !=5){
+ 	cout << "Podaj liczbe: ";
+ 	cin >> liczba;
+ 	
+ } 
+  
+  cout << endl << endl;
+  short liczba2;
+  do{
+  	 cout << "Podaj liczbe: ";
+  	 cin >> liczba2;
+  	 }while(liczba2 != 5 );
+
+
+
+
+}
