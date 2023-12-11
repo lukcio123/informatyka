3025fa362515685600a8efaa7ec95533180b6ca6
lukcio123 <lukaszizotow76@gmail.com>
2023-12-11 14:58:34 +0100

    Komentarz

---
+++ lekcja7/Untitled1.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+char tablica[100];
+cout << "Podaj imie i nazwisko: "<<endl;
+cin.getline(tablica,100);
+cout <<"Twoje dane osobowe: "<<tablica<<endl;
+for(int i=0;i<100;i++){
+	if(tablica[i]==' ') {
+		
+	}
+}
+
+ cout << "Pozycja spacji:10"<<endl;
+ 
+}
